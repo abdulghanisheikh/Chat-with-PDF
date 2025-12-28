d19369a17ab5d2384c46ace297ebc85e7b5ec6c6
Abdul Ghani <ghanisheikh26@gmail.com>
2025-12-28 23:00:50 +0530

    done with the vectore emdeddings

--- server/queue/pdfWorker.js
+++ server/queue/pdfWorker.js
@@ -5,26 +5,30 @@ config({
 });
 import {Worker} from "bullmq";
 import {connection} from "./redis.js";
-import {OpenAIEmbeddings} from "@langchain/openai";
 import {QdrantVectorStore} from "@langchain/qdrant";
 import {PDFLoader} from "@langchain/community/document_loaders/fs/pdf";
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+import {CharacterTextSplitter} from "@langchain/textsplitters";
 
 const worker=new Worker("pdf-queue",async(job)=>{
     const data=job.data;
     const loader=new PDFLoader(data.filePath);
     const docs=await loader.load(); //array of documents
-    const embeddings=new OpenAIEmbeddings({
-        apiKey:process.env.OPENAI_API_KEY,
-        model:"text-embedding-3-small"
+    const splitter=new CharacterTextSplitter({
+        chunkSize:500,
+        chunkOverlap:0
     });
-    const vectorStore=await QdrantVectorStore.fromExistingCollection(
-        embeddings,
-        {
-            url:process.env.QDRANT_URL,
-            collectionName:"pdf-content"
-        });
-    await vectorStore.addDocuments(docs);
-    console.log(`All docs are stored in vector store`);
+    const chunks=await splitter.splitDocuments(docs);
+    const embeddings=new HuggingFaceInferenceEmbeddings({
+        apiKey:process.env.HF_API_KEY,
+        model:"sentence-transformers/all-MiniLM-L6-v2"
+    });
+    const vectorStore=await QdrantVectorStore.fromExistingCollection(embeddings,{
+        collectionName:"pdf-content",
+        url:process.env.QDRANT_URL 
+    });
+    await vectorStore.addDocuments(chunks);
+    console.log(`documents uploaded in vector store`);
 },{connection});
 
 worker.on("completed",(job)=>{
